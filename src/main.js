1200e12515e9d7baa91ceb18886f61f180112fb2
Digital-Koi <ryanavery99@yahoo.co.uk>
2026-01-29 22:41:00 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+console.log('WJJF North Coast loaded.');
+
+
+// Mobile Navigation Toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    // Close menu when clicking a link
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
+    });
+}
+
