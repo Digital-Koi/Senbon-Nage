cff1491da2482bea705ae589f1d473230db2fae3
Digital-Koi <ryanavery99@yahoo.co.uk>
2026-02-02 15:12:56 +0000

    Fix: Add instructors.html to build input

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        instructors: resolve(__dirname, 'instructors.html'),
+      },
+    },
   },
 })
